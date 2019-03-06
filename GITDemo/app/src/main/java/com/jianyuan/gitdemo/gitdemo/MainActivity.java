a755de5c646b29a604b04d79890edc9fa21640ce
sunmingshun <sunmingshun@jyan.net.cn>
2019-03-06 16:55:42 +0800

    Add banner andr qrcode testcode

--- GITDemo/app/src/main/java/com/jianyuan/gitdemo/gitdemo/MainActivity.java
+++ GITDemo/app/src/main/java/com/jianyuan/gitdemo/gitdemo/MainActivity.java
@@ -1,30 +1,106 @@
 package com.jianyuan.gitdemo.gitdemo;
 
+import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
+import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.jianyuan.gitdemo.gitdemo.util.QRCodeUtil;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
+import com.youth.banner.Transformer;
+import com.youth.banner.listener.OnBannerListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends BaseActivity {
     private int i=0;
     private TextView mHelloTV;
+    private Banner mBanner;
+    private MyImageLoader mMyImageLoader;
+    private List<String> list_path = new ArrayList<>();
+    private List<String> list_title = new ArrayList<>();
+    private ImageView mQrcodeIV ;
+    private Bitmap mQrcodeBitmap;//生成的二维码
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        MyApplication.mMainActivity = MainActivity.this;
         setContentView(R.layout.activity_main);
+        String sn = MyApplication.getSn(MainActivity.this);
+        if(!sn.equals("")){
+            Log.e("设备sn>>>:",sn);
+        }
+        Log.e("设备sn>>>:",sn);
+        initData();
         init();
     }
-
+    private void initData() {
+        list_path.add("https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=e9873bfca944ad342eea8f81e09220cc/a8ec8a13632762d08fa73daea8ec08fa513dc602.jpg");
+        list_path.add("https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=e9873bfca944ad342eea8f81e09220cc/a8ec8a13632762d08fa73daea8ec08fa513dc602.jpg");
+        list_path.add("https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=e9873bfca944ad342eea8f81e09220cc/a8ec8a13632762d08fa73daea8ec08fa513dc602.jpg");
+        list_path.add("https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao/wh%3D600%2C800/sign=e9873bfca944ad342eea8f81e09220cc/a8ec8a13632762d08fa73daea8ec08fa513dc602.jpg");
+        list_title.add("");
+        list_title.add("");
+        list_title.add("");
+        list_title.add("");
+    }
     private void init() {
-        mHelloTV = findViewById(R.id.tv_hello);
-        mHelloTV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(MainActivity.this,"Hello world",Toast.LENGTH_SHORT).show();
-            }
-        });
+        String content = "https://gss0.baidu.com/94o3dSag_xI4khGko9WTAnF6hhy/zhidao";
+        mQrcodeIV = findViewById(R.id.iv_qrcode);
+        mQrcodeBitmap = QRCodeUtil.createQRCodeBitmap(content, 350, 350, "UTF-8",
+                "H", "1", Color.BLACK, Color.WHITE, null, 0.2F, null);
+        mQrcodeIV.setImageBitmap(mQrcodeBitmap);
+        mMyImageLoader = new MyImageLoader();
+        mBanner = findViewById(R.id.banner);
+        //设置样式，里面有很多种样式可以自己都看看效果
+        mBanner.setBannerStyle(BannerConfig.CIRCLE_INDICATOR);
+        //设置图片加载器
+        mBanner.setImageLoader(mMyImageLoader);
+        //设置轮播的动画效果,里面有很多种特效,可以都看看效果。
+        mBanner.setBannerAnimation(Transformer.Default);
+        //轮播图片的文字
+        mBanner.setBannerTitles(list_title);
+        //设置轮播间隔时间
+        mBanner.setDelayTime(5000);
+        //设置是否为自动轮播，默认是true
+        mBanner.isAutoPlay(true);
+        //设置指示器的位置，小点点，居中显示
+        mBanner.setIndicatorGravity(BannerConfig.CIRCLE_INDICATOR_TITLE);
+        //设置图片加载地址
+        mBanner.setImages(list_path)
+                //轮播图的监听
+                .setOnBannerListener(new OnBannerListener() {
+                    @Override
+                    public void OnBannerClick(int position) {
+                        Toast.makeText(MainActivity.this, "你点了第" + (position + 1) + "张轮播图", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                //开始调用的方法，启动轮播图。
+                .start();
+    }
 
+    public void acceptPush(String pushData){
+          boolean isTop =  isActivityTop(MainActivity.class,MainActivity.this);
+          if(isTop){
+              Toast.makeText(MainActivity.this, "topName>>>"+isTop, Toast.LENGTH_SHORT).show();
+              Log.e("topName>>>:",isTop+"");
+
+          }
+    }
+
+    private void getToken(){
+
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MyApplication.mMainActivity = null;
     }
 }
--- GITDemo/app/src/main/java/com/jianyuan/gitdemo/gitdemo/MyApplication.java
+++ GITDemo/app/src/main/java/com/jianyuan/gitdemo/gitdemo/MyApplication.java
@@ -1,13 +1,25 @@
 package com.jianyuan.gitdemo.gitdemo;
 
-
+import android.Manifest;
 import android.app.Application;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.telephony.TelephonyManager;
 import android.util.DisplayMetrics;
 import android.util.Log;
+import android.widget.Toast;
+
 import com.umeng.commonsdk.UMConfigure;
+import com.umeng.message.IUmengRegisterCallback;
+import com.umeng.message.PushAgent;
+import com.umeng.message.UmengMessageHandler;
+import com.umeng.message.entity.UMessage;
 
 public class MyApplication extends Application {
+    public static MainActivity mMainActivity;
+    private static final String TAG = MyApplication.class.getName();
     public static int screenWidth;
     public static int screenHeight;
     public static String IP_PORT;
@@ -15,20 +27,102 @@ public class MyApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
-        UMConfigure.init(this,UMConfigure.DEVICE_TYPE_PHONE, "你在友盟申请的UMENG_MESSAGE_SECRET");
-//        PushAgent mPushAgent = PushAgent.getInstance(this);
-//        mPushAgent.setResourcePackageName("你应用的applicationId");
-//        mPushAgent.register(new IUmengRegisterCallback() {
+        UMConfigure.init(this, UMConfigure.DEVICE_TYPE_PHONE, "aff55c079ca0dfa3937637287414ebb5");
+        PushAgent mPushAgent = PushAgent.getInstance(this);
+        mPushAgent.setResourcePackageName("com.jianyuan.gitdemo.gitdemo");
+        mPushAgent.register(new IUmengRegisterCallback() {
+            @Override
+            public void onSuccess(String s) {
+                Log.e("获取token成功:", s);
+            }
+
+            @Override
+            public void onFailure(String s, String s1) {
+                Log.e("获取token失败:", s + "且s1=" + s1);
+            }
+        });
+//        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+//
+//            /**
+//             * 自定义通知栏样式的回调方法
+//             */
 //            @Override
-//            public void onSuccess(String s) {
-//                Log.e("获取token成功:",s);
+//            public Notification getNotification(Context context, UMessage msg) {
+//                switch (msg.builder_id) {
+//                    case 1:
+//                        Notification.Builder builder = new Notification.Builder(context);
+//                        RemoteViews myNotificationView = new RemoteViews(context.getPackageName(),
+//                                R.layout.notification_view);
+//                        myNotificationView.setTextViewText(R.id.notification_title, msg.title);
+//                        myNotificationView.setTextViewText(R.id.notification_text, msg.text);
+//                        myNotificationView.setImageViewBitmap(R.id.notification_large_icon, getLargeIcon(context, msg));
+//                        myNotificationView.setImageViewResource(R.id.notification_small_icon,
+//                                getSmallIconId(context, msg));
+//                        builder.setContent(myNotificationView)
+//                                .setSmallIcon(getSmallIconId(context, msg))
+//                                .setTicker(msg.ticker)
+//                                .setAutoCancel(true);
+//
+//                        return builder.getNotification();
+//                    default:
+//                        //默认为0，若填写的builder_id并不存在，也使用默认。
+//                        return super.getNotification(context, msg);
+//                }
+//            }
+//        };
+//        mPushAgent.setMessageHandler(messageHandler);
+//
+//        UmengNotificationClickHandler notificationClickHandler = new UmengNotificationClickHandler(){
+//            @Override
+//            public void dealWithCustomAction(Context context, UMessage msg){
+//                Log.e(TAG,"click");
 //            }
 //
-//            @Override
-//            public void onFailure(String s, String s1) {
-//                Log.e("获取token失败:",s+"且s1="+s1);
-//            }
-//        });
+//        };
+//
+//        mPushAgent.setNotificationClickHandler(notificationClickHandler);
+
+        UmengMessageHandler messageHandler = new UmengMessageHandler() {
+            @Override
+            public void dealWithCustomMessage(final Context context, final UMessage msg) {
+                new Handler(getMainLooper()).post(new Runnable() {
+                    @Override
+                    public void run() {
+                        // 对于自定义消息，PushSDK默认只统计送达。若开发者需要统计点击和忽略，则需手动调用统计方法。
+//                        boolean isClickOrDismissed = true;
+//                        if(isClickOrDismissed) {
+//                            //自定义消息的点击统计
+//                            UTrack.getInstance(getApplicationContext()).trackMsgClick(msg);
+//                        } else {
+//                            //自定义消息的忽略统计
+//                            UTrack.getInstance(getApplicationContext()).trackMsgDismissed(msg);
+//                        }
+                        if (mMainActivity != null) {
+                            mMainActivity.acceptPush(msg.custom);
+                        }
+                        Log.e("获取透传消息msg.custom:" + msg.custom, msg.toString());
+                        Toast.makeText(context, msg.custom, Toast.LENGTH_LONG).show();
+                    }
+                });
+            }
+        };
+        mPushAgent.setMessageHandler(messageHandler);
+    }
+
+    public static String getSn(Context context) {
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+        if (ActivityCompat.checkSelfPermission(context, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return "";
+        }
+        String sn = tm.getSimSerialNumber();
+        return sn;
     }
 
     public static void getAndroiodScreenProperty(Context context) {
